4edfb1f9b91d90c2a1283b0bb4144b6aee747852
yaoyj <yaoyj@163.com>
2021-11-05 13:57:29 +0800

    add function

--- Test/Test/源.cpp
+++ Test/Test/源.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
+int add(int a, int b) {
+	return a + b;
+}
 using namespace std;
 int main() {
-
+	cout << add(2, 3) << endl;
 	cout << "init git" << endl;
 	return 0;
 }
