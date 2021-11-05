6c3b68f398f8015b0a9cffd73792e4e546e2c13c
yaoyj <yaoyj@163.com>
2021-11-05 14:08:42 +0800

    d1

---
+++ Test/Test/源.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+//d1
+int add(int a, int b) {
+	return a + b;
+}
+using namespace std;
+
+int main() {
+	cout << add(2, 3) << endl;
+	cout << "init git" << endl;
+	return 0;
+}
